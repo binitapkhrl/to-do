082b54e6ad9c5acc7b0700746ac1edf86b7b98ba
Binitaaaaaa <binitapokharel33@gmail.com>
2025-12-22 13:05:57 +0545

    cleaning up code

--- to_doapp/lib/pages/todo_page.dart
+++ to_doapp/lib/pages/todo_page.dart
@@ -13,16 +13,29 @@ class TodoPage extends ConsumerWidget {
       body: todoAsync.when(
         loading: () => Center(child: CircularProgressIndicator()),
         error: (e, _) => Center(child: Text(e.toString())),
-        data: (todos) => ListView.builder(
+        data:(todos) {
+  if (todos.isEmpty) {
+    return const Center(child: Text('No tasks found!'));
+  }
+         return ListView.builder(
           itemCount: todos.length,
+          // itemBuilder: (context, index) {
+          //   return CheckboxListTile(
+          //     title: Text(todos[index].title),
+          //     value: todos[index].completed,
+          //     onChanged: null, // read-only for now
+          //   );
+          // },
           itemBuilder: (context, index) {
-            return CheckboxListTile(
-              title: Text(todos[index].title),
-              value: todos[index].completed,
-              onChanged: null, // read-only for now
-            );
+  final todo = todos[index]; 
+  return CheckboxListTile(
+    title: Text(todo.title),
+    value: todo.completed,
+    onChanged: (val) {
+      // You could call a notifier here to toggle completion
+    },);
           },
-        ),
+        );}
       ),
     );
   }
