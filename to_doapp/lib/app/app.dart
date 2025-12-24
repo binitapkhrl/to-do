cd01e37d8e0b7b15c65e5268e47cf3a8fc6b2f40
Binitaaaaaa <binitapokharel33@gmail.com>
2025-12-24 15:33:02 +0545

    handelling state through riverpod and integrating riverpod and beamer

--- to_doapp/lib/app/app.dart
+++ to_doapp/lib/app/app.dart
@@ -1,20 +1,46 @@
 
+// import 'package:flutter/material.dart';
+// import 'package:beamer/beamer.dart';
+// import 'package:to_doapp/app/router/app_router.dart';
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp.router(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Todo Architecture',
+//       routerDelegate: AppRouter.routerDelegate,
+//       routeInformationParser: BeamerParser(),
+//       backButtonDispatcher:
+//           BeamerBackButtonDispatcher(delegate: AppRouter.routerDelegate),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:beamer/beamer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:to_doapp/app/router/app_router.dart';
 
-class MyApp extends StatelessWidget {
+// 2. Change StatelessWidget to ConsumerWidget
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  // 3. Add WidgetRef ref to the build method
+  Widget build(BuildContext context, WidgetRef ref) {
+    
+    // 4. Watch the router provider
+    final delegate = ref.watch(routerDelegateProvider);
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Todo Architecture',
-      routerDelegate: AppRouter.routerDelegate,
+      // 5. Use the delegate from Riverpod
+      routerDelegate: delegate,
       routeInformationParser: BeamerParser(),
-      backButtonDispatcher:
-          BeamerBackButtonDispatcher(delegate: AppRouter.routerDelegate),
+      backButtonDispatcher: BeamerBackButtonDispatcher(delegate: delegate),
     );
   }
 }
--- to_doapp/lib/app/router/app_router.dart
+++ to_doapp/lib/app/router/app_router.dart
@@ -1,29 +1,38 @@
 import 'package:beamer/beamer.dart';
 import 'package:to_doapp/app/location/todo_location.dart';
 import 'package:to_doapp/app/router/routes.dart';
+import 'package:to_doapp/features/state/login_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// --- MOVE THIS OUTSIDE THE CLASS ---
+final routerDelegateProvider = Provider<BeamerDelegate>((ref) {
+  return BeamerDelegate(
+    initialPath: Routes.login,
+    locationBuilder: BeamerLocationBuilder(
+      beamLocations: [TodoLocation()],
+    ).call,
+    guards: [
+      // PROTECT TODOS: Redirect to login if not authenticated
+      BeamGuard(
+        pathPatterns: ['/todos', '/todos/*'],
+        check: (context, state) => ref.watch(isAuthenticatedProvider),
+        beamToNamed: (origin, target) => Routes.login,
+      ),
+      
+      // Prevent logged-in users from going back to Login page
+      BeamGuard(
+        pathPatterns: [Routes.login],
+        check: (context, state) => !ref.watch(isAuthenticatedProvider),
+        beamToNamed: (origin, target) => Routes.todos,
+      ),
+    ],
+  );
+});
 
 class AppRouter {
-  // Route paths (delegated to `Routes` to avoid duplication)
+  // Static paths remain here
   static const String home = Routes.login;
   static const String todos = Routes.todos;
   static const String todoDetail = Routes.todoDetail;
   static String todoDetailPath(String username, int id) => Routes.todoDetailPath(username, id);
-
-static final routerDelegate = BeamerDelegate(
-  initialPath: Routes.login,
-  locationBuilder: BeamerLocationBuilder(
-    beamLocations: [TodoLocation()],
-  ).call,
-  guards: [
-    // PROTECT TODOS: Redirect to login if not authenticated
-    BeamGuard(
-      pathPatterns: ['/todos*'],
-      check: (context, state) {
-        return true; 
-      },
-      beamToNamed: (origin, target) => Routes.login,
-    ),
-  ],
-);
 }
-
